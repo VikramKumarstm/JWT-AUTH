07662e27b76eee036a6184dba33be930be3b9a05
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-29 08:28:03 +0530

    logged Out user

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -127,8 +127,34 @@ const getUser = async (req, res) => {
     }
 }
 
+const logout = (req, res) => {
+    try {
+
+        const cookieOption = {
+            expires: new Date(),
+            httpOnly: true
+        };
+
+        res.cookie("token", null, cookieOption);
+
+        res.status(200).json({
+            success: true,
+            message: 'Logged out'
+        })
+        
+    } catch (error) {
+        console.error("ERROR :", error);
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+        
+    }
+}
+
 module.exports = {
     signup,
     signin,
-    getUser
+    getUser,
+    logout
 }
