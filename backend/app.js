b2ea23a294b94b7318bc373fce8b54d6ebc816da
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-29 19:51:41 +0530

    feat cors

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,16 @@ const authRouter = require('./router/authRoute.js');
 const databaseConnect = require('./config/databaseConfig.js');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true
+}))
 
 //database connection
 databaseConnect();
